86a7d088a5076ba8e2dfd6057cfc9970281fe004
BangalaDur667 <Inismon.grossedaronnenoire@gmail.com>
2023-02-05 18:27:45 +0100

    zazaza

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 const menuButton = document.querySelector(".menu-button");
 const navMenu = document.querySelector("nav");
+const screenDiv = document.getElementById("f-div");
 
 menuButton.addEventListener("click", toggleMenu);
 
@@ -7,3 +8,67 @@ function toggleMenu() {
   menuButton.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
+
+if (window.screen.width >= 1800) {
+  screenDiv.classList.replace(
+    "full-page-div-smallscreen",
+    "full-page-div-bigscreen"
+  );
+}
+
+const slidesPos = document.querySelector(".slides");
+const leftChevron = document.getElementById("left-chevron");
+const rightChevron = document.getElementById("right-chevron");
+var slideNumber = 0;
+
+function checkSlide(page) {
+  if (page == 0) {
+    slidesPos.classList.toggle("pos1");
+  }
+  if (page == 1) {
+    slidesPos.classList.toggle("pos2");
+  }
+  if (page == 2) {
+    slidesPos.classList.toggle("pos3");
+  }
+  if (page == 3) {
+    slidesPos.classList.toggle("pos4");
+  }
+  if (page == 4) {
+    slidesPos.classList.toggle("pos5");
+  }
+  if (page == 5) {
+    slidesPos.classList.toggle("pos6");
+  }
+  if (page == 6) {
+    slidesPos.classList.toggle("pos7");
+  }
+  if (page == 7) {
+    slidesPos.classList.toggle("pos8");
+  }
+}
+
+function leftPress() {
+  if (slideNumber == 0) {
+    slideNumber = 0;
+    console.log(slideNumber);
+  } else {
+    checkSlide(slideNumber);
+    slideNumber--;
+    console.log(slideNumber);
+    checkSlide(slideNumber);
+  }
+}
+
+function rightPress() {
+  if (slideNumber == 6) {
+    slideNumber = 6;
+    console.log(slideNumber);
+  } else {
+    checkSlide(slideNumber);
+    slideNumber++;
+    checkSlide(slideNumber);
+  }
+}
+leftChevron.addEventListener("click", leftPress);
+rightChevron.addEventListener("click", rightPress);
